7f88cf97baa49ce5aee4f5d1625969c87a807554
孙博浩 <bohao.sun@mail.hypers.com>
2024-11-18 18:33:05 +0800

    fix: 把部分远程图片下载到本地

--- packages/easy-email-core/src/utils/getImg.ts
+++ packages/easy-email-core/src/utils/getImg.ts
@@ -1,14 +1,14 @@
 import { ImageManager } from './ImageManager';
+import facebookIcon from '../images/facebook-icon.png';
+import googleIcon from '../images/google-icon.png';
+import twitterIcon from '../images/twitter-icon.png';
 
 const defaultImagesMap = {
   IMAGE_01:
     'https://easy-email-m-ryan.vercel.app/images/ffddc3db-3aae-4d73-ac9c-e1263641f7b4-03c89c34-49a4-4d45-b289-4d2261158cbe.png',
-  IMAGE_02:
-    'https://easy-email-m-ryan.vercel.app/images/acbae5eb-efa4-4eb6-866c-f421e740b713-ad3c92b1-9cdb-4a7b-aad3-75ad809db8a3.png',
-  IMAGE_03:
-    'https://easy-email-m-ryan.vercel.app/images/98520d6c-5cef-449e-bcbf-6316ccec2088-e8780361-0deb-4896-895e-e690c886cdf0.png',
-  IMAGE_04:
-    'https://easy-email-m-ryan.vercel.app/images/b064f705-34ba-4400-975e-9dd0cec21c30-cc9aa158-56bd-4bf1-b532-72390d25c864.png',
+  IMAGE_02: facebookIcon,
+  IMAGE_03: googleIcon,
+  IMAGE_04: twitterIcon,
   IMAGE_59:
     'https://easy-email-m-ryan.vercel.app/images/8e0e07e2-3f84-4426-84c1-2add355c558b-image.png',
   IMAGE_09:
